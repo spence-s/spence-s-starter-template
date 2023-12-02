b6fecd8ce253f8ea7e174b24941bfe413bc08680
Spencer Snyder <sasnyde2@gmail.com>
2023-12-02 16:39:55 -0500

    feat: add tests setup for starter

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+
 export function helloWorld() {
 	console.log(`Hello World! 🌎
 
---
+++ test/index.test.ts
@@ -0,0 +1,6 @@
+import test from 'ava';
+import {helloWorld} from '../src/index.ts';
+
+test('helloWorld is a function', t => {
+	t.is(typeof helloWorld, 'function');
+});
