7643a1c2fe9d1813e7590f032c2de1db65364c43
Spencer Snyder <sasnyde2@gmail.com>
2025-05-10 19:48:23 -0400

    chore: adjust lint-staged

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.md,!test/**/*.md': 'prettier --check',
+  '*.md,!test/**/*.md': 'prettier --write',
   './package.json':
     'prettier --write --plugin=prettier-plugin-packagejson ./package.json',
   '*.{js,ts}': 'xo --fix',
