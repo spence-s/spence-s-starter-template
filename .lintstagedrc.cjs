8e739d919dc62770dd8e50c4774d417ffa25bcd5
Spencer Snyder <sasnyde2@gmail.com>
2025-05-10 19:43:12 -0400

    chore: bump deps + xo next

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,8 +1,6 @@
 module.exports = {
   '*.md,!test/**/*.md': 'prettier --check',
-  './package.json': [
-    'npmPkgJsonLint ./package.json',
+  './package.json':
     'prettier --write --plugin=prettier-plugin-packagejson ./package.json',
-  ],
   '*.{js,ts}': 'xo --fix',
 };
