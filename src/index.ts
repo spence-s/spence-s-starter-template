8000053e84fc3d35cb13b88f3da0e1675b53a273
Spencer Snyder <sasnyde2@gmail.com>
2025-05-10 20:12:52 -0400

    fix: hello world text

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ To get started:
 
 1) run 'npm install'
 2) run 'npm run update' to update dependencies to their latest versions
-3) edit 'src/index.ts' and run 'npm run dev' again to see your changes.
-4) run 'npm run dev:watch' to automatically recompile your code when you make changes.
-5) run 'npm run test' to run the tests in 'src/index.test.ts'
+3) run 'npm run dev' to start developing and automatically recompile and run your code when you make changes.
+4) run 'npm run test' to run the tests in 'src/index.test.ts'
+5) run 'npm run release' to release a new version of your package to npm
 `);
 }
